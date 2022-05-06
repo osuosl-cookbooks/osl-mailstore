d2695c2986363d9661f700360b96bb03262a3226
Lance Albertson <lance@osuosl.org>
2022-05-06 16:46:36 -0700

    Initial commit
    Signed-off-by: Lance Albertson <lance@osuosl.org>

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name              'osl-mailstore'
+maintainer        'Oregon State University'
+maintainer_email  'chef@osuosl.org'
+license           'All Rights Reserved'
+description       'Installs/Configures osl-mailstore'
+issues_url        'https://github.com/osuosl-cookbooks/osl-mailstore/issues'
+source_url        'https://github.com/osuosl-cookbooks/osl-mailstore'
+chef_version      '>= 16.0'
+version           '0.1.0'
+
+supports          'centos', '~> 7.0'
+supports          'centos', '~> 8.0'
