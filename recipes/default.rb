f381e082b8aad4abe885aca7d6d45c7db7c84179
David Headrick <daviddjh@osuosl.org>
2022-05-13 16:56:55 -0700

    Getting postfixadmin going

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,3 +15,54 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# include_recipe php
+# include_recipe mysql
+
+# Following this tutorial: https://linuxize.com/post/set-up-an-email-server-with-postfixadmin/
+
+group 'vmail' do
+  gid 5000
+end  
+
+user 'vmail' do
+  comment 'Owner of all mailboxes. Used to access emails on this server'
+  home    '/var/mail/vmail'
+  uid     5000
+  gid     5000
+  shell   '/usr/sbin/nologin'
+end
+
+# For testing
+
+user 'www-data' do
+  comment 'Php user'
+  uid     5001
+  gid     5000
+end
+
+directory '/var/www'
+
+remote_file '/var/www/postfixadmin.tar.gz' do
+  source "https://downloads.sourceforge.net/project/postfixadmin/postfixadmin/postfixadmin-#{node['postfixadmin']['version']}/postfixadmin-#{node['postfixadmin']['version']}.tar.gz"
+  mode '0755'
+end
+
+archive_file '/var/www/postfixadmin.tar.gz' do
+  destination '/var/www/tmp'
+  owner 'www-data'
+  not_if { ::File.exist?('/var/www/postfixadmin') }
+end
+
+bash "Move and rename postfixadmin" do
+  code <<-EOL
+  mv /var/www/tmp/postfixadmin-#{node['postfixadmin']['version']} /var/www/postfixadmin
+  EOL
+  not_if { ::File.exist?('/var/www/postfixadmin') }
+end
+  
+template "/var/www/postfixadmin/config.local.php" do
+  source 'config.local.php.erb'
+end
+
+
